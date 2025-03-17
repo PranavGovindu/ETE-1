26edbd053c296a614839108ee1b2c73d1e72d46c
PranavGovindu <violaze25@gmail.com>
2025-03-17 23:04:53 +0530

    data ingestion

--- src/logger/__init__.py
+++ src/logger/__init__.py
@@ -1,33 +1,42 @@
 import logging
 import os
+import sys
 from datetime import datetime
-logger=logging.getLogger()
-logger.setLevel('DEBUG')
 
-root_dir=os.path.dirname(os.path.abspath(os.path.join(os.path.dirname(__file__),'../')))
-log_dir_path=os.path.join(root_dir,'logs')
-os.makedirs(log_dir_path,exist_ok=True)
-log_file_path=os.path.join(f"{ datetime.now().strftime("%m_%d_%y_%H_%M_%S") }.log")
+# Get the root directory
+root_dir = os.path.dirname(os.path.abspath(os.path.join(os.path.dirname(__file__), '../')))
+log_dir_path = os.path.join(root_dir, 'logs')
+os.makedirs(log_dir_path, exist_ok=True)
+
+# Create log file name with timestamp
+log_file_name = f"{datetime.now().strftime('%m_%d_%y_%H_%M_%S')}.log"
+log_file_path = os.path.join(log_dir_path, log_file_name)
 
 def configure_logger():
     """
     Configure logger
     """
-    logger=logging.getLogger()
+    logger = logging.getLogger()
+    logger.setLevel(logging.DEBUG)
     
-    logger.setLevel('DEBUG')
+    # Create formatter
+    formatter = logging.Formatter("[%(asctime)s] %(name)s - %(levelname)s - %(message)s")
     
-    formatt=logging.Formatter("f[%(asctime)s ] %(name)s -  %(levelname)s - %(message)s")
-    
-    file_handler=logging.FileHandler(log_file_path,encoding="utf-8")
-    file_handler.setFormatter(formatt)
+    # File handler
+    file_handler = logging.FileHandler(log_file_path, encoding="utf-8")
+    file_handler.setFormatter(formatter)
     file_handler.setLevel(logging.INFO)
     
-    console_handler=logging.StreamHandler(log_file_path)
-    console_handler.setFormatter(formatt)
+    # Console handler
+    console_handler = logging.StreamHandler(sys.stdout)  # Use sys.stdout instead of file path
+    console_handler.setFormatter(formatter)
     console_handler.setLevel(logging.INFO)
     
+    # Add handlers to logger
     logger.addHandler(file_handler)
     logger.addHandler(console_handler)
     
-configure_logger()
+    return logger
+
+# Configure and get logger
+logger = configure_logger()
